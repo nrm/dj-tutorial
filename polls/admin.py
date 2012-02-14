06736924d3cfbe95d9b7d667ea4201572f6963e9
Ilya <rbetra@gmail.com>
2012-02-14 14:58:12 +0400

    change pub_date and question in admin site

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,8 @@
 from polls.models import Poll
 from django.contrib import admin
 
-admin.site.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question"]
+
+admin.site.register(Poll, PollAdmin)
 
