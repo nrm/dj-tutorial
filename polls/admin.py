312ab3987eecb1604f94071673d837cc8f855d0e
Ilya <rbetra@gmail.com>
2012-02-14 15:05:32 +0400

    example modefide admin page

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,13 @@ from polls.models import Poll
 from django.contrib import admin
 
 class PollAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question"]
+    #fields = ["pub_date", "question"]
+    fieldsets = [
+        (None,                  {"fields": ["question"]}),
+        ("Date information",    {"fields": ["pub_date"], "classes": ["collapse"]}),
+        #("Question field",      {"fields": ["question"]}),
+    ]
+
 
 admin.site.register(Poll, PollAdmin)
 
